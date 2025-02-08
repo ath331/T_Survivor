1a5724d02cabaddfdd3fefac130f684345f4516b
JaeWoo <kimdn0520@naver.com>
2025-02-08 13:53:12 +0900

    BasePopupHandler, PopupManager 추가 Title canvas 크기 조정

---
+++ AtClientFramework/Assets/Scripts/99.Utils/BasePopupHandler.cs
@@ -0,0 +1,173 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+/// <summary>
+/// 게임 내에 팝업에 공통되는 처리를 담당하는 클래스
+/// 팝업들은 필수적으로 BasePopupHandler를 상속을 받는다.
+/// </summary>
+public class BasePopupHandler : MonoBehaviour
+{
+    private bool initialized;
+    private int lockCount;
+
+    private Canvas canvas;
+    public Canvas Canvas
+    {
+        get
+        {
+            if (!initialized) Initialize();
+            return canvas;
+        }
+    }
+
+    private Transform animationRoot;
+    protected virtual Transform AnimationRoot
+    {
+        get
+        {
+            if (!initialized) Initialize();
+            return animationRoot;
+        }
+    }
+
+    private void Initialize()
+    {
+        initialized = true;
+        animationRoot = transform.Find("Canvas/PopupCommon");
+        canvas = GetComponentInChildren<Canvas>(true);
+    }
+
+    protected virtual bool UseBlocker => true;             // 배경에 안눌리게 하는 blocker 를 채울건지
+    protected virtual bool HandleClickOutside => false;    // 검은배경 눌렀을때 반응 할건지
+    protected virtual bool IsPointerDownHandler => true;   // 검은배경 터치 이벤트 타입 (IPointerDownHandler / IPointerClickHandler)
+
+    protected virtual bool HandleEscapeKey => true;
+    protected virtual float CurtainAlpha => .8f;
+
+    private const int BaseSortingOrder = 0;
+    protected virtual int SortingGroupBonusOrder => 0;
+    protected virtual string PopupSoundEnter => "popup_in";
+    protected virtual string PopupSoundLeave => "popup_out";
+
+    //팝업을 현재 닫을 수 있는 상태인가?
+    protected virtual bool CanEscape()
+    {
+        // TODO : 적절한 코드
+        // 현재 띄워져 있는 팝업이랑 같은 팝업인지..? 체크 등
+
+        return true;
+    }
+
+    /// <summary>
+    /// 팝업이 띄워지기 전 (AnimationIn 전)
+    /// </summary>
+    /// <param name="param"></param>
+    public virtual void OnBeforeEnter(object param)
+    {
+        //if (!string.IsNullOrEmpty(PopupSoundEnter)) SoundManager.Play(PopupSoundEnter);
+
+        int popupCount = PopupManager.GetPopupStackCount();
+
+        if (!ReferenceEquals(null, Canvas))
+        {
+            Canvas.worldCamera = Camera.main;
+            Canvas.sortingOrder = BaseSortingOrder + 1000 * popupCount;
+
+            // if (UseBlocker) UIBlocker.Setup(GetInstanceID(), Canvas.sortingLayerName, Canvas.sortingOrder - 1, CurtainAlpha, IsPointerDownHandler);
+        }
+    }
+
+    /// <summary>
+    /// 팝업이 띄워진 후 (AnimationIn 후)
+    /// </summary>
+    /// <param name="param"></param>
+    public virtual void OnAfterEnter(object param)
+    {
+
+    }
+
+    /// <summary>
+    /// 팝업이 꺼지기 전 (AnimationOut 전)
+    /// </summary>
+    public virtual void OnBeforeLeave()
+    {
+
+    }
+
+    /// <summary>
+    /// 팝업이 꺼진 후 (AnimtionOut 후)
+    /// </summary>
+    public virtual void OnAfterLeave()
+    {
+        
+    }
+
+    protected virtual void OnClickOutside()
+    {
+        
+    }
+
+    public virtual void OnClickEscape()
+    {
+        
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    public virtual void OnClickClose()
+    {
+
+    }
+
+    public virtual void OnClickCloseReturnBool(bool b)
+    {
+
+    }
+
+    /// <summary>
+    /// TODO : 팝업이 등장할때의 애니메이션 (팝업이 그냥 나오는게 아니라 점점 커지면서 나온다던지 그런 느낌!)
+    /// </summary>
+    /// <returns></returns>
+    public virtual async UniTask AnimationIn()
+    {
+        if (!AnimationRoot) return;
+
+        var orgScale = Vector3.one;
+        var sequence = DOTween.Sequence()
+            .Append(AnimationRoot.DOScale(orgScale * 1.02f, .12f).SetEase(Ease.OutQuart).From(orgScale))
+            .Append(AnimationRoot.DOScale(orgScale * .99f, .08f).SetEase(Ease.OutQuart))
+            .Append(AnimationRoot.DOScale(orgScale, .05f));
+
+        await sequence.AwaitForCompletion();
+    }
+
+    public virtual UniTask AnimationOut()
+    {
+        return UniTask.CompletedTask;
+    }
+
+    /// <summary>
+    /// 팝업의 정렬 순서를 설정하는 메서드
+    /// </summary>
+    /// <param name="sortingOrder">새로운 정렬 순서</param>
+    public void AddSortingOrder(int sortingOrder)
+    {
+
+    }
+
+    /// <summary>
+    /// 현재 팝업의 정렬 순서를 반환하는 메서드
+    /// </summary>
+    /// <returns>정렬 순서</returns>
+    public int GetSortingOrder()
+    {
+        return Canvas.sortingOrder;
+    }
+}
--- AtClientFramework/Assets/Scripts/Managers/PopupManager.cs
+++ AtClientFramework/Assets/Scripts/Managers/PopupManager.cs
@@ -1,18 +1,172 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using UnityEngine.Assertions;
+using System.Linq;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
-public class PopupManager : MonoBehaviour
+public static class PopupManager
 {
-    // Start is called before the first frame update
-    void Start()
+    private class PopupItem
+    {
+        public readonly BasePopupHandler popupHandler;
+        public readonly Action<object> closeCallback;
+        public readonly object param;
+
+        public PopupItem(BasePopupHandler popupHandler, Action<object> closeCallback, object param)
+        {
+            this.popupHandler = popupHandler;
+            this.closeCallback = closeCallback;
+            this.param = param;
+        }
+    }
+
+    /// <summary>
+    /// Popup들의 부모오브젝트
+    /// </summary>
+    private static GameObject popupParent;
+    private static GameObject PopupParent
+    {
+        get
+        {
+            if (popupParent) return popupParent;
+            popupParent = new GameObject("Popup Parent");
+            Object.DontDestroyOnLoad(popupParent);
+            return popupParent;
+        }
+    }
+
+    /// <summary>
+    /// TODO : 현재 화면이 등장하는 팝업을 관리한다.
+    /// stack, queue, linkedlist 뭐든 좋음.
+    /// </summary>
+    private static readonly LinkedList<PopupItem> popupStack = new LinkedList<PopupItem>();
+
+
+    public static int GetPopupStackCount() => popupStack.Count;
+
+    // 현재 맨 앞에 열려있는 팝업을 가져온다.
+    //public static BasePopupHandler CurrentPopup
+    //{
+    //    get
+    //    {
+    //        if (popupStack.Count == 0) return null;
+
+    //        return popupStack.Last().popupHandler;
+    //    }
+    //}
+
+    /// <summary>
+    /// 팝업을 바로 보여주고싶을때 쓴다.
+    /// </summary>
+    /// <param name="popupName"></param>
+    /// <param name="param"></param>
+    /// <param name="callback"></param>
+    public static void ShowPopup(string popupName, object param = null, System.Action<object> callback = null)
+    {
+        // TODO :
+    }
+
+    /// <summary>
+    /// 팝업을 await ShowAsync 로 보여주고 그 이후 실행은 잠깐 멈출때 씀
+    /// </summary>
+    /// <param name="popupName"></param>
+    /// <param name="param"></param>
+    /// <param name="closeCallback"></param>
+    public static async void ShowAsync(string popupName, object param = null, Action<object> closeCallback = null)
+    {
+        var done = false;
+
+        try
+        {
+            // ShowProcessAsync가 종료되면 done을 true로 설정
+            ShowProcessAsync(popupName, param, closeParam =>
+            {
+                closeCallback?.Invoke(closeParam);  // closeCallback 실행
+                done = true;  // closeCallback이 실행된 후 done을 true로 설정
+            }).Forget();
+
+            // done이 true가 될 때까지 대기
+            await UniTask.WaitUntil(() => done);
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError($"Error during popup show process: {ex.Message}");
+
+            done = true;  // 예외가 발생해도 done을 true로 설정하여 대기 종료
+        }
+
+        await UniTask.WaitUntil(() => done);
+    }
+
+    private static async UniTask ShowProcessAsync(string popupName, object param, System.Action<object> closeCallback = null)
+    {
+        BasePopupHandler popupInstance = null;
+
+        // PopupName 프리팹 중 BasePopupHandler 스크립트를 가진 프리팹을 가져옴. 
+
+        // 꺼둔다.
+
+        // 만들어서 PopupParent 하위로 넣어준다.
+
+        // 팝업 스택에 추가한다.
+
+        // 씬이 바뀌었다면 기다린다. (씬이 바뀐후 Show)
+
+        // OnEnable() -> Start()
+
+        // 팝업이 등장하기 전
+
+        // 팝업이 꺼지는 애니메이션
+        await popupInstance.AnimationOut();
+
+        // 팝업이 등장한 후
+
+    }
+
+    /// <summary>
+    /// 스택에 있는 모든 팝업을 끈다.
+    /// </summary>
+    /// <param name="param"></param>
+    /// <param name="destroy"></param>
+    public static async void CloseAll(object param = null)
+    {
+       
+    }
+
+    /// <summary>
+    /// 스택에 있는 팝업을 이름을 통해 첫번째만 찾아서 끈다.
+    /// </summary>
+    /// <param name="popupName"></param>
+    /// <param name="param"></param>
+    /// <param name="destroy"></param>
+    public static async void ClosePopupByName(string popupName, object param = null)
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public static async void ClosePopup(object param = null)
     {
-        
+        // 팝업스택 최상위 것을 가져온다.
+
+        // 지운다.
+
+        // 해당 팝업을 끈다.
+    }
+
+    private static async UniTask CloseProcessAsync(PopupItem popupItem, object param)
+    {
+        // 팝업이 꺼지기 전
+
+        // 팝업이 꺼지는 애니메이션
+
+        // 팝업이 꺼진 후
+
+        // popup 오브젝트를 삭제 시킨다.
+
+        // CloseCallback 이 있다면 실행한다.
+
     }
 }
