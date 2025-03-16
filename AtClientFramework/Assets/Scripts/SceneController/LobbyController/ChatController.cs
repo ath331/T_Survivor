a8f05c7d36511d916d3567dada6ebf735258d813
Taeyang <xodid2627@naver.com>
2025-03-16 14:45:33 +0900

    채팅 진행 중

--- AtClientFramework/Assets/Scripts/SceneController/LobbyController/ChatController.cs
+++ AtClientFramework/Assets/Scripts/SceneController/LobbyController/ChatController.cs
@@ -13,19 +13,25 @@ public class ChatController : MonoBehaviour
     private const int LimitChatPrefab = 20;
     private const int LimitChatLength = 50;
 
+
     // 해야 될 것 inputfield에 입력해서 엔터/입력 버튼을 누를 때 채팅을 전송하고 content안에 자식 프리팹을 생성해서 넣어야 한다!
 
-    private void Update()
+    private void Start()
+    {
+        inputField.onEndEdit.AddListener(Send_Message);
+    }
+
+    private void Submit(string input)
     {
         if (inputField.isFocused && Input.GetKeyDown(KeyCode.Return))   // 채팅창을 누르고 엔터키를 누를 시
         {
-            Send_Message();
+            Send_Message(input);
         }
     }
 
-    public void Send_Message()
+    public void Send_Message(string input)
     {
-        string msg = inputField.text.Trim();
+        string msg = input.Trim();
 
         // 비어있는 메시지는 보내지 않음.
         if (string.IsNullOrEmpty(msg)) return;
@@ -33,6 +39,8 @@ public class ChatController : MonoBehaviour
         GameObject chatText = ObjectPoolManager.Instance.Get("ChatText");
         chatText.transform.parent = content.transform;
         chatText.SetActive(true);
+
+        chatText.GetComponent<ChatText>().ReceiveMessage(msg);
 
         C_Chat pkt = new C_Chat();
 
--- AtClientFramework/Assets/Scripts/SceneController/LobbyController/ChatText.cs
+++ AtClientFramework/Assets/Scripts/SceneController/LobbyController/ChatText.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ChatText : MonoBehaviour
 {
+    [SerializeField] private TMP_Text text;
+
+    public void ReceiveMessage(string input)
+    {
+        text.text = input;
+    }
     private void OnDestroy()
     {
         ObjectPoolManager.Instance.Return("ChatText", this.gameObject);
